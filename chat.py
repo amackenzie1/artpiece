88476145d4a875a0126f956a4ae98b37928bd970
amackenzie1 <amackenzie@stenoa.com>
2024-03-06 20:46:48 -0500

    fixing chat

--- chat.py
+++ chat.py
@@ -1,5 +1,9 @@
+from threading import Thread
+
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          GenerationConfig, TextIteratorStreamer, TextStreamer,
+                          pipeline)
 
 # Initialize the tokenizer and model
 adapter_model_id = "dandelion4/annie-llama-mistral-7b"
@@ -15,6 +19,48 @@ if torch.cuda.is_available():
 
 # Initialize conversation history
 conversation_history = ""
+
+
+def generate(instruction):
+        if not instruction:
+            return
+        prompt = instruction.strip()
+        batch = tokenizer(prompt, return_tensors="pt", add_special_tokens=True)
+
+        model.eval()
+        with torch.no_grad():
+            generation_config = GenerationConfig(
+                repetition_penalty=1.1,
+                max_new_tokens=1024,
+                temperature=0.9,
+                top_p=0.95,
+                top_k=40,
+                bos_token_id=tokenizer.bos_token_id,
+                eos_token_id=tokenizer.eos_token_id,
+                pad_token_id=tokenizer.pad_token_id,
+                do_sample=True,
+                use_cache=True,
+                return_dict_in_generate=True,
+                output_attentions=False,
+                output_hidden_states=False,
+                output_scores=False,
+            )
+            streamer = TextIteratorStreamer(tokenizer)
+            generation_kwargs = {
+                "inputs": batch["input_ids"].to(cfg.device),
+                "generation_config": generation_config,
+                "streamer": streamer,
+            }
+
+            thread = Thread(target=model.generate, kwargs=generation_kwargs)
+            thread.start()
+
+            all_text = ""
+
+            for new_text in streamer:
+                all_text += new_text
+                yield all_text
+
 
 # Chat loop
 while True:
@@ -32,11 +78,21 @@ while True:
         inputs = inputs.to("cuda")
 
     # Generate a response
-    response_ids = model.generate(inputs, max_length=100, pad_token_id=tokenizer.eos_token_id,
-                                  top_p=0.95, top_k=50, temperature=0.7,
-                                  num_return_sequences=1)
-    response = tokenizer.decode(response_ids[0], skip_special_tokens=True)
-
+    response_ids = model.generate(inputs,
+                                  repetition_penalty=1.1,
+                max_new_tokens=1024,
+                temperature=0.9,
+                top_p=0.95,
+                top_k=40,
+                bos_token_id=tokenizer.bos_token_id,
+                eos_token_id=tokenizer.eos_token_id,
+                pad_token_id=tokenizer.pad_token_id,
+                do_sample=True,
+                use_cache=True, 
+                num_return_sequences=1)
+    raw_response = tokenizer.decode(response_ids[0], skip_special_tokens=True)
+    response = raw_response.split(conversation_history)[-1]
+    response = response.split("####Andrew:")[0]
     # Print the model's response
     print(f"Annie: {response}")
 
